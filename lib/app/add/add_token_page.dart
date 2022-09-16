b4e63b23b4fb3d50b1e1b83065095c10124b249a
Maciej Jagodzinski <maciejjagodzinskizip@gmail.com>
2022-09-16 17:29:11 +0200

    Change adding coins logic and UI

--- lib/app/add/add_token_page.dart
+++ lib/app/add/add_token_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rektometer/app/add/cubit/add_token_cubit.dart';
+import 'package:rektometer/app/add/search_delegate.dart';
 import 'package:rektometer/data/remote_data_sources/portfolio_remote_data_source.dart';
 import 'package:rektometer/data/remote_data_sources/token_remote_data_source.dart';
-import 'package:rektometer/models/token_list_model.dart';
 import 'package:rektometer/repositories/portfolio_repository.dart';
 import 'package:rektometer/repositories/token_list_repository.dart';
 
@@ -23,7 +23,7 @@ class _AddTokenPageState extends State<AddTokenPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add some shitcoins'),
+        title: const Text('Add some coins'),
       ),
       body: BlocProvider(
         create: (context) => AddTokenCubit(
@@ -66,82 +66,5 @@ class _AddTokenPageState extends State<AddTokenPage> {
         ),
       ),
     );
-  }
-}
-
-class SearchTokenModelDelegate extends SearchDelegate {
-  SearchTokenModelDelegate(this.tokenList);
-
-  final List<TokenListModel> tokenList;
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-          onPressed: () {
-            query = '';
-          },
-          icon: const Icon(Icons.clear))
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-        onPressed: () {
-          close(context, query);
-        },
-        icon: const Icon(Icons.arrow_back));
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (final token in tokenList) {
-      if (token.name.toLowerCase() == (query.toLowerCase())) {
-        matchQuery.add(token.id);
-      }
-    }
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: ListView.builder(
-          itemCount: matchQuery.length,
-          itemBuilder: (context, index) {
-            final result = matchQuery[index];
-            return ListTile(
-              tileColor: Colors.blue.shade200,
-              title: Text(
-                query,
-                style: const TextStyle(fontSize: 30),
-              ),
-              subtitle: Text('ID:$result'),
-              onTap: () {
-                close(context, result);
-              },
-            );
-          }),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (final token in tokenList) {
-      if (token.name.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(token.name);
-      }
-    }
-    return ListView.builder(
-        itemCount: matchQuery.length,
-        itemBuilder: (context, index) {
-          final suggestion = matchQuery[index];
-          return ListTile(
-            title: Text(suggestion),
-            onTap: () {
-              query = suggestion;
-              showResults(context);
-            },
-          );
-        });
   }
 }
---
+++ lib/app/add/search_delegate.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rektometer/app/add/cubit/add_token_cubit.dart';
+import 'package:rektometer/data/remote_data_sources/portfolio_remote_data_source.dart';
+import 'package:rektometer/data/remote_data_sources/token_remote_data_source.dart';
+import 'package:rektometer/models/token_list_model.dart';
+import 'package:rektometer/repositories/portfolio_repository.dart';
+import 'package:rektometer/repositories/token_list_repository.dart';
+
+class SearchTokenModelDelegate extends SearchDelegate {
+  SearchTokenModelDelegate(this.tokenList);
+
+  final List<TokenListModel> tokenList;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, query);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (final token in tokenList) {
+      if (token.name.toLowerCase() == (query.toLowerCase())) {
+        matchQuery.add(token.id);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          final result = matchQuery[index];
+          return BlocProvider(
+            create: (context) => AddTokenCubit(
+                TokenListRepository(TokenListRemoteDataSource()),
+                PortfolioRepository(PortfolioRemoteDataSource())),
+            child: BlocBuilder<AddTokenCubit, AddTokenState>(
+                builder: (context, state) {
+              return Center(
+                child: Column(children: [
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).colorScheme.primary,
+                        blurRadius: 20,
+                      ),
+                    ]),
+                    height: 140,
+                    width: 300,
+                    child: Card(
+                      color: Theme.of(context).backgroundColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(children: [
+                          Text(
+                            'Add ${result.toUpperCase()} to portfolio?',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          const SizedBox(height: 10),
+                          IconButton(
+                            onPressed: () {
+                              context
+                                  .read<AddTokenCubit>()
+                                  .addToken(id: result);
+                            },
+                            icon: Icon(Icons.check_box,
+                                size: 50,
+                                color: Theme.of(context).colorScheme.primary),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ),
+                ]),
+              );
+            }),
+          );
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (final token in tokenList) {
+      if (token.name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(token.name);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          final suggestion = matchQuery[index];
+          return ListTile(
+              title: Text(suggestion),
+              onTap: () {
+                query = suggestion;
+                showResults(context);
+              });
+        });
+  }
+}
