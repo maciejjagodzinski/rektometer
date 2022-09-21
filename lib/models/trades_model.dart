9a0bb0ef7ad453290e053551b2ec9ec2374ca27d
Maciej Jagodzinski <maciejjagodzinskizip@gmail.com>
2022-09-21 19:25:26 +0200

    Add intl date formatter

--- lib/models/trades_model.dart
+++ lib/models/trades_model.dart
@@ -1,17 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class TradeModel {
   TradeModel({
+    required this.tradeDocumentId,
     required this.tradeTokenId,
     required this.volume,
     required this.price,
     required this.date,
     required this.type,
   });
-
+  final String tradeDocumentId;
   final String tradeTokenId;
   final double volume;
   final double price;
   final Timestamp date;
   final String type;
+
+  String tradeDateFormatted() {
+    return DateFormat.yMd().format(date.toDate());
+  }
 }
