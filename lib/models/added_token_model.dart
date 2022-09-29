2b0f9d026efb6cb04693651d6ae5a9c416c22328
Maciej Jagodzinski <maciejjagodzinskizip@gmail.com>
2022-09-29 19:43:24 +0200

    Delete unused added token model

--- lib/models/added_token_model.dart
+++
@@ -1,9 +0,0 @@
-class AddedTokenModel {
-  AddedTokenModel({
-    required this.addedTokenId,
-    required this.investmentDocumentId,
-  });
-
-  final String addedTokenId;
-  final String investmentDocumentId;
-}
